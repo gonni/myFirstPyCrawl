e0c1330e0029fadaa94d9a57ea48f29966c47ef7
김영곤 (Jeff Kim) <jeff.y.kim@onestorecorp.com>
2024-08-27 18:24:49 +0900

    daily commit

--- job/PredictApi.py
+++ job/PredictApi.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api, Resource
+import ml.KosdaPredictPycaret as kpp
 
 app = Flask(__name__)
 api = Api(app)
@@ -9,6 +10,14 @@ api = Api(app)
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
+
+@api.route('/predKosdaq')
+class PredictKosdaq(Resource):
+    def get(self):
+        res = kpp.predict()
+        pred = float(res['prediction_label'].iloc[0])
+        return {'kosdaq3d': pred }
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8088)
